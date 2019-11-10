30b1a14e6057b8c32961be1c5992b15bf22adab4
Zhoye <512238273@qq.com>
2019-11-11 00:24:11 +0800

    发货人统计

--- web/App_Code/User/FHRMag.cs
+++ web/App_Code/User/FHRMag.cs
@@ -248,5 +248,253 @@ public class FHRMag
             }
         }
 
-    }        
+    }
+
+    [CSMethod("GetFHRTJList")]
+    public object GetFHRTJList(int pagnum, int pagesize, string cx_beg, string cx_end, string cx_fgcs, string cx_hb, string cx_city, string cx_lxgs, string cx_lxts, string cx_lxcs)
+    {
+        using (DBConnection dbc = new DBConnection())
+        {
+            try
+            {
+                int cp = pagnum;
+                int ac = 0;
+
+                string where = "";
+
+                if (!string.IsNullOrEmpty(cx_beg))
+                {
+                    where += " and  AddTime>='" + Convert.ToDateTime(cx_beg).ToString("yyyy-MM-dd") + "'";
+                }
+                if (!string.IsNullOrEmpty(cx_end))
+                {
+                    where += " and AddTime<='" + Convert.ToDateTime(cx_end).AddDays(1).ToString("yyyy-MM-dd") + "'";
+                }
+
+                string where1 = "";
+                if (!string.IsNullOrEmpty(cx_fgcs))
+                {
+                    where1 += " and  a.num<'" + Convert.ToInt32(cx_fgcs)+ "'";
+                }
+
+                string where2 = "";
+                if (!string.IsNullOrEmpty(cx_city))
+                {
+                    where2 += " and  a.dqbm=" + dbc.ToSqlValue(cx_city);
+                }
+
+                string where3 = "";
+                if (!string.IsNullOrEmpty(cx_lxgs) && !string.IsNullOrEmpty(cx_lxts) && !string.IsNullOrEmpty(cx_lxcs))
+                {
+                    int gs=Convert.ToInt32(cx_lxgs);
+
+                    int ts = Convert.ToInt32(cx_lxts);
+
+                    int cs = Convert.ToInt32(cx_lxcs);
+
+                    where3 += @" except
+                        select * from (";
+                    for (int i = 0; i < gs; i++)
+                    {
+                        where3 += @"select BuyUserID from (
+                                select BuyUserID,count(1) num from tb_b_order where datediff(day,AddTime,dateadd(day,0,getdate())) >= 0 and datediff(day,AddTime,dateadd(day,"+(-i*ts)+@",getdate())) < "+ts+@" and ZhiFuZT = 1 and Status = 0 group by BuyUserID
+                            ) a where a.num > " + cs;
+                        if (i != (gs - 1)) {
+                            where3 += "intersect";
+                        }
+                    }
+                    where3 += ") a";
+                       
+                }
+
+                string where4 = "";
+                if (!string.IsNullOrEmpty(cx_hb))
+                {
+                    where4 += " and " + dbc.C_GT("money", Convert.ToDecimal(cx_hb));
+                }
+
+                string str = @"
+                select * from (
+                     select userid,username,case LEN(DqBm) when 6 then DqBm else '320400' end dqbm from tb_b_user
+                     where UserID in (
+                        select BuyUserID from (
+                            select BuyUserID,count(1) num from tb_b_order where  ZhiFuZT = 1 and Status = 0 group by BuyUserID
+                        ) a where 1=1 "+where1+@"
+                            " + where3 + @"
+                    )
+                ) a where 1=1 " + where2 + @" and UserID not in (
+                    select userid from (
+                        select sum(money) money,userid from tb_b_redenvelope where isuse = 0 group by userid
+                    ) a where 1=1 " + where4 + ")";
+
+                //开始取分页数据
+                System.Data.DataTable dtPage = new System.Data.DataTable();
+                dtPage = dbc.GetPagedDataTable(str, pagesize, ref cp, out ac);
+
+                return new { dt = dtPage, cp = cp, ac = ac };
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+
+
+    [CSMethod("GetFHRTJListToFile", 2)]
+    public byte[] GetFHRTJListToFile(string cx_beg, string cx_end, string cx_fgcs, string cx_hb, string cx_city, string cx_lxgs, string cx_lxts, string cx_lxcs)
+    {
+        using (DBConnection dbc = new DBConnection())
+        {
+            try
+            {
+                Workbook workbook = new Workbook(); //工作簿
+                Worksheet sheet = workbook.Worksheets[0]; //工作表
+                Cells cells = sheet.Cells;//单元格
+
+                //样式2
+                Style style2 = workbook.Styles[workbook.Styles.Add()];
+                style2.HorizontalAlignment = TextAlignmentType.Left;//文字居中
+                style2.Font.Name = "宋体";//文字字体
+                style2.Font.Size = 14;//文字大小
+                style2.Font.IsBold = true;//粗体
+                style2.IsTextWrapped = true;//单元格内容自动换行
+                style2.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin; //应用边界线 左边界线
+                style2.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin; //应用边界线 右边界线
+                style2.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin; //应用边界线 上边界线
+                style2.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin; //应用边界线 下边界线
+                style2.IsLocked = true;
+
+                //样式3
+                Style style4 = workbook.Styles[workbook.Styles.Add()];
+                style4.HorizontalAlignment = TextAlignmentType.Left;//文字居中
+                style4.Font.Name = "宋体";//文字字体
+                style4.Font.Size = 11;//文字大小
+                style4.Borders[BorderType.LeftBorder].LineStyle = CellBorderType.Thin;
+                style4.Borders[BorderType.RightBorder].LineStyle = CellBorderType.Thin;
+                style4.Borders[BorderType.TopBorder].LineStyle = CellBorderType.Thin;
+                style4.Borders[BorderType.BottomBorder].LineStyle = CellBorderType.Thin;
+
+                cells.SetRowHeight(0, 20);
+                cells[0, 0].PutValue("手机号");
+                cells[0, 0].SetStyle(style2);
+                cells.SetColumnWidth(0, 20);
+                cells[0, 1].PutValue("城市");
+                cells[0, 1].SetStyle(style2);
+                cells.SetColumnWidth(1, 20);
+
+                string where = "";
+
+                if (!string.IsNullOrEmpty(cx_beg))
+                {
+                    where += " and  AddTime>='" + Convert.ToDateTime(cx_beg).ToString("yyyy-MM-dd") + "'";
+                }
+                if (!string.IsNullOrEmpty(cx_end))
+                {
+                    where += " and AddTime<='" + Convert.ToDateTime(cx_end).AddDays(1).ToString("yyyy-MM-dd") + "'";
+                }
+
+                string where1 = "";
+                if (!string.IsNullOrEmpty(cx_fgcs))
+                {
+                    where1 += " and  a.num<'" + Convert.ToInt32(cx_fgcs) + "'";
+                }
+
+                string where2 = "";
+                if (!string.IsNullOrEmpty(cx_city))
+                {
+                    where2 += " and  a.dqbm=" + dbc.ToSqlValue(cx_city);
+                }
+
+                string where3 = "";
+                if (!string.IsNullOrEmpty(cx_lxgs) && !string.IsNullOrEmpty(cx_lxts) && !string.IsNullOrEmpty(cx_lxcs))
+                {
+                    int gs = Convert.ToInt32(cx_lxgs);
+
+                    int ts = Convert.ToInt32(cx_lxts);
+
+                    int cs = Convert.ToInt32(cx_lxcs);
+
+                    where3 += @" except
+                        select * from (";
+                    for (int i = 0; i < gs; i++)
+                    {
+                        where3 += @"select BuyUserID from (
+                                select BuyUserID,count(1) num from tb_b_order where datediff(day,AddTime,dateadd(day,0,getdate())) >= 0 and datediff(day,AddTime,dateadd(day," + (-i * ts) + @",getdate())) < " + ts + @" and ZhiFuZT = 1 and Status = 0 group by BuyUserID
+                            ) a where a.num > " + cs;
+                        if (i != (gs - 1))
+                        {
+                            where3 += "intersect";
+                        }
+                    }
+                    where3 += ") a";
+
+                }
+
+                string where4 = "";
+                if (!string.IsNullOrEmpty(cx_hb))
+                {
+                    where4 += " and " + dbc.C_GT("money", Convert.ToDecimal(cx_hb));
+                }
+
+                string str = @"
+                select * from (
+                     select userid,username,case LEN(DqBm) when 6 then DqBm else '320400' end dqbm from tb_b_user
+                     where UserID in (
+                        select BuyUserID from (
+                            select BuyUserID,count(1) num from tb_b_order where  ZhiFuZT = 1 and Status = 0 group by BuyUserID
+                        ) a where 1=1 " + where1 + @"
+                            " + where3 + @"
+                    )
+                ) a where 1=1 " + where2 + @" and UserID not in (
+                    select userid from (
+                        select sum(money) money,userid from tb_b_redenvelope where isuse = 0 group by userid
+                    ) a where 1=1 " + where4 + ")";
+
+                //开始取分页数据
+                System.Data.DataTable dt = new System.Data.DataTable();
+                dt = dbc.ExecuteDataTable(str);
+
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    if (dt.Rows[i]["username"] != null && dt.Rows[i]["username"].ToString() != "")
+                    {
+                        cells[i + 1, 0].PutValue(dt.Rows[i]["username"]);
+                    }
+                    cells[i + 1, 0].SetStyle(style4);
+
+                    if (dt.Rows[i]["dqbm"] != null && dt.Rows[i]["dqbm"].ToString() != "")
+                    {
+                        if (dt.Rows[i]["dqbm"].ToString() == "320500")
+                        {
+                            cells[i + 1, 1].PutValue("苏州");
+                        }
+                        else if (dt.Rows[i]["dqbm"].ToString() == "320400")
+                        {
+                            cells[i + 1, 1].PutValue("常州");
+                        }
+                        else if (dt.Rows[i]["dqbm"].ToString() == "320200")
+                        {
+                            cells[i + 1, 1].PutValue("无锡");
+                        }
+                    }
+                    else
+                    {
+                        cells[i + 1, 1].PutValue("常州");
+                    }
+                    cells[i + 1, 1].SetStyle(style4);
+                }
+
+                MemoryStream ms = workbook.SaveToStream();
+                byte[] bt = ms.ToArray();
+                return bt;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
 }
---
+++ web/JS/FHR/FHRTJ.js
@@ -0,0 +1,296 @@
+﻿var pageSize = 15;
+var cx_name;
+var cx_dj;
+var id = "";
+//************************************数据源*****************************************
+var store = createSFW4Store({
+    data: [],
+    pageSize: pageSize,
+    total: 1,
+    currentPage: 1,
+    fields: [
+       { name: 'UserID' },
+       { name: 'username' },
+       { name: 'dqbm'}
+       
+    ],
+    onPageChange: function (sto, nPage, sorters) {
+        GetFHRTJList(nPage);
+    }
+});
+//************************************数据源*****************************************
+
+//************************************页面方法***************************************
+function GetFHRTJList(nPage) {
+    if (Ext.getCmp("cx_fgcs").getValue() < 0) {
+        Ext.MessageBox.alert('提示', '复购次数不能小于0！');
+        return;
+    }
+    if (Ext.getCmp("cx_hb").getValue() < 0) {
+        Ext.MessageBox.alert('提示', '红包剩余金额不能小于0！');
+        return;
+    }
+    if (Ext.getCmp("cx_lxgs").getValue() != "" && Ext.getCmp("cx_lxgs").getValue() != null
+        && Ext.getCmp("cx_lxts").getValue() != "" && Ext.getCmp("cx_lxts").getValue() != null
+        && Ext.getCmp("cx_lxcs").getValue() != "" && Ext.getCmp("cx_lxcs").getValue() != null) {
+       
+    } else if (Ext.getCmp("cx_lxgs").getValue() == null&& Ext.getCmp("cx_lxts").getValue() == null&& Ext.getCmp("cx_lxcs").getValue() == null) { } else {
+        Ext.MessageBox.alert('提示', '请填写完整连续h个n天的订单数在m次以内！');
+        return;
+    }
+
+
+    CS('CZCLZ.FHRMag.GetFHRTJList', function (retVal) {
+        store.setData({
+            data: retVal.dt,
+            pageSize: pageSize,
+            total: retVal.ac,
+            currentPage: retVal.cp
+        });
+    }, CS.onError, nPage, pageSize, Ext.getCmp("cx_beg").getValue(), Ext.getCmp("cx_end").getValue(), Ext.getCmp("cx_fgcs").getValue(), Ext.getCmp("cx_hb").getValue()
+    , Ext.getCmp("cx_city").getValue(), Ext.getCmp("cx_lxgs").getValue(), Ext.getCmp("cx_lxts").getValue(), Ext.getCmp("cx_lxcs").getValue());
+}
+
+//************************************页面方法***************************************
+
+
+//************************************主界面*****************************************
+Ext.onReady(function () {
+    Ext.define('YhView', {
+        extend: 'Ext.container.Viewport',
+
+        layout: {
+            type: 'fit'
+        },
+
+        initComponent: function () {
+            var me = this;
+            me.items = [
+                {
+                    xtype: 'gridpanel',
+                    id: 'usergrid',
+                    title: '',
+                    store: store,
+                    columnLines: true,
+                    columns: [Ext.create('Ext.grid.RowNumberer'),
+                         {
+                             xtype: 'gridcolumn',
+                             dataIndex: 'id',
+                             sortable: false,
+                             menuDisabled: true,
+                             hidden: true,
+                             flex: 1
+                         },
+                            {
+                                xtype: 'gridcolumn',
+                                dataIndex: 'username',
+                                sortable: false,
+                                menuDisabled: true,
+                                flex: 1,
+                                text: "手机号"
+                            },
+                             {
+                                 xtype: 'gridcolumn',
+                                 dataIndex: 'dqbm',
+                                 sortable: false,
+                                 menuDisabled: true,
+                                 flex: 1,
+                                 text: "城市",
+                                renderer: function (value, cellmeta, record, rowIndex, columnIndex, store) {
+                                    var str = "";
+                                    if (value == "320500") {
+                                        str = "苏州";
+                                    } else if (value == "320200") {
+                                        str = "无锡";
+                                    } else {
+                                        str = "常州";
+                                    }
+                                    return str;
+                                }
+                             },
+
+                    ],
+                    viewConfig: {
+
+                    },
+                    dockedItems: [
+                                {
+                                    xtype: 'toolbar',
+                                    dock: 'top',
+                                    items: [
+                                        {
+                                            id: 'cx_beg',
+                                            xtype: 'datefield',
+                                            fieldLabel: '开始时间',
+                                            format: 'Y-m-d',
+                                            labelWidth: 60,
+                                            width: 190
+                                        },
+                                        {
+                                            id: 'cx_end',
+                                            xtype: 'datefield',
+                                            format: 'Y-m-d',
+                                            fieldLabel: '结束时间',
+                                            labelWidth: 60,
+                                            width: 190
+                                        },
+                                        {
+                                            id: 'cx_fgcs',
+                                            xtype: 'numberfield',
+                                            fieldLabel: '复购次数',
+                                            allowDecimals: false,
+                                            allowNegative: false,
+                                            minValue:0,
+                                            labelWidth: 60,
+                                            width: 190
+                                        },
+                                        {
+                                            xtype: 'displayfield',
+                                            value: "次以内"
+                                        },
+                                        {
+                                            id: 'cx_hb',
+                                            xtype: 'numberfield',
+                                            fieldLabel: '红包剩余金额',
+                                            allowNegative: false,
+                                            minValue: 0,
+                                            labelWidth: 80,
+                                            width: 190
+                                        },
+                                         {
+                                             xtype: 'combobox',
+                                             id: 'cx_city',
+                                             width: 160,
+                                             fieldLabel: '城市',
+                                             editable: false,
+                                             labelWidth: 40,
+                                             store: Ext.create('Ext.data.Store', {
+                                                 fields: [
+                                                    { name: 'val' },
+                                                    { name: 'txt' }
+                                                 ],
+                                                 data: [
+                                                     {
+                                                         'val': '',
+                                                         'txt': '全部',
+                                                     },
+                                                     {
+                                                         'val': '320400',
+                                                         'txt': '常州',
+                                                     },
+                                                     {
+                                                         'val': '320500',
+                                                         'txt': '苏州',
+                                                     },
+                                                     {
+                                                         'val': '320200',
+                                                         'txt': '无锡',
+                                                     },
+                                                 ]
+                                             }),
+                                             queryMode: 'local',
+                                             displayField: 'txt',
+                                             valueField: 'val',
+                                             value: ''
+                                         }
+                                    ]
+                                },
+                                {
+                                    xtype: 'toolbar',
+                                    dock: 'top',
+                                    items: [
+                                        {
+                                            xtype: 'displayfield',
+                                            value: "连续"
+                                        },
+                                        {
+                                            id: 'cx_lxgs',
+                                            xtype: 'numberfield',
+                                            allowDecimals: false,
+                                            allowNegative: false,
+                                            minValue: 0,
+                                            width: 100
+                                        },
+                                        {
+                                            xtype: 'displayfield',
+                                            value: "个"
+                                        },
+                                        {
+                                            id: 'cx_lxts',
+                                            xtype: 'numberfield',
+                                            allowDecimals: false,
+                                            allowNegative: false,
+                                            minValue: 0,
+                                            width: 100
+                                        },
+                                        {
+                                            xtype: 'displayfield',
+                                            value: "天的订单数在"
+                                        },
+                                         {
+                                             id: 'cx_lxcs',
+                                             xtype: 'numberfield',
+                                             allowDecimals: false,
+                                             allowNegative: false,
+                                             minValue: 0,
+                                             width: 100
+                                         },
+                                         {
+                                             xtype: 'displayfield',
+                                             allowDecimals: false,
+                                             allowNegative: false,
+                                             minValue: 0,
+                                             value: "次以内"
+                                         },
+                                        {
+                                            xtype: 'buttongroup',
+                                            title: '',
+                                            items: [
+                                                {
+                                                    xtype: 'button',
+                                                    iconCls: 'search',
+                                                    text: '查询',
+                                                    handler: function () {
+                                                        if (privilege("财务报表_发货人统计_查看")) {
+                                                            GetFHRTJList(1);
+                                                        }
+                                                    }
+                                                }
+                                            ]
+                                        },
+                                         {
+                                             xtype: 'buttongroup',
+                                             title: '',
+                                             items: [
+                                                    {
+                                                        xtype: 'button',
+                                                        text: '导出',
+                                                        handler: function () {
+                                                            if (privilege("财务报表_发货人统计_导出")) {
+                                                                DownloadFile("CZCLZ.FHRMag.GetFHRTJListToFile", "发货人统计.xls", Ext.getCmp("cx_beg").getValue(), Ext.getCmp("cx_end").getValue(), Ext.getCmp("cx_fgcs").getValue(), Ext.getCmp("cx_hb").getValue()
+                                                                , Ext.getCmp("cx_city").getValue(), Ext.getCmp("cx_lxgs").getValue(), Ext.getCmp("cx_lxts").getValue(), Ext.getCmp("cx_lxcs").getValue());
+                                                            }
+                                                        }
+                                                    }]
+                                         }
+                                    ]
+                                },
+                                {
+                                    xtype: 'pagingtoolbar',
+                                    displayInfo: true,
+                                    store: store,
+                                    dock: 'bottom'
+                                }
+                    ]
+                }
+            ];
+            me.callParent(arguments);
+        }
+    });
+
+    new YhView();
+
+    GetFHRTJList(1);
+
+})
+//************************************主界面*****************************************
